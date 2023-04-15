f93a4bf87b60f65e41f01cd3baff7366b9a87450
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2023-04-16 05:10:40 +0900

    API追加

--- confluence/api.py
+++ confluence/api.py
@@ -19,13 +19,38 @@ class Api:
         self.session = sessions.BaseUrlSession(base_url=base_url + "/rest/api/")
         self.session.auth = (username, password)
 
+        self.content = self.Content(self.session)
+
+    class Content:
+        def __init__(self, session: requests.Session) -> None:
+            self.session = session
+
+        def get_content(self, query_params: Optional[QueryParams] = None):
+            return self.session.get(f"content", params=query_params)
+
+        def get_content_by_id(self, id: str, query_params: Optional[QueryParams] = None):
+            response = self.session.get(f"content/{id}", params=query_params)
+            return response
+
+        def delete(self, id: str, query_params: Optional[QueryParams] = None):
+            response = self.session.delete(f"content/{id}", params=query_params)
+            return response
+
+        def get_history(self, id: str, query_params: Optional[QueryParams] = None):
+            response = self.session.get(f"content/{id}/history", params=query_params)
+            return response.json()
+
+        def search(self, query_params: Optional[QueryParams] = None):
+            response = self.session.get(f"content/search", params=query_params)
+            return response.json()
+
     class ContentById:
         class ChildAttachment:
             def __init__(self, session: requests.Session, content_id: str) -> None:
                 self.session = session
                 self.content_id = content_id
 
-            def get_attachments(self, query_params: Optional[QueryParams]=None) -> dict[str, Any]:
+            def get_attachments(self, query_params: Optional[QueryParams] = None) -> dict[str, Any]:
                 response = self.session.get(f"content/{self.content_id}/child/attachment", params=query_params)
                 print(f"{self.session=}")
                 print(f"{response.request.url=}, {response.status_code=}")
